8849fe57ab75e029ae1133d4acb9fd4a51ea8e63
Rafael Felisbino Rocha da Silva <72631228+RafaelFelisbino-hub@users.noreply.github.com>
2020-11-20 19:19:05 -0300

    Update Passagens de Trem.c

--- Passagens de Trem.c
+++ Passagens de Trem.c
@@ -8,11 +8,6 @@ Para liberar os assentos, você deverá iniciar a venda de passagens pelo vagão
 vender os assentos do vagão 2, e assim sucessivamente. Mas se a passagem for da classe econômica, você deverá iniciar do vagão 6. O comprador não
 pode escolher o assento que ele deseja.
 
-Calcular o valor das passagens por comprador. Passagem da classe executiva = R$ 58,00. 
-Passagem da classe econômica = R$ 35,00. Perguntar qual a forma de pagamento. 
-Se for dinheiro deve analisar se o valor pago precisa de troco, se sim, informar o valor do troco. Se for cartão (crédito ou débito) 
-você deverá dar um desconto de R$ 1,75 por passagem.
-
 Criar um menu de opções onde poderá comprar a passagem, visualizar a quantidade total de passagens vendidas por classe,
 visualizar a quantidade de assentos disponíveis por vagão.
 
@@ -24,12 +19,11 @@ Criar uma interface amigável, utilize todos os recursos aprendidos até hoje na
 
 char passageiros[200];
 int op, i, passagem1[45];
-int vagao1 = 45, vagao2 = 45, vagao3 = 45, vagao4 = 45, op1 = 0, op2 = 0, op3 = 0, pass1 = 0, pass2 = 0, pass3 = 0, pass4 = 0;
+int vagao1 = 45, vagao2 = 45, vagao3 = 45, vagao4 = 45, op1 = 0, op2 = 0, op3 = 0, pass1 = 0, pass2 = 0, pass3 = 0, pass4 = 0, op4 = 0;
 int vagao5 = 45, pass5 = 0, vagao6 = 45, pass6 = 0, vagao7 = 45, pass7 = 0;
 int vagao8 = 45, pass8 = 0, vagao9 = 45, pass9 = 0, vagao10 = 45, pass10 = 0;
 int pVendidas = 0;
 int recebePass1 = 0, recebePass2 = 0, recebePass3 = 0, recebePass4 = 0, recebePass5 = 0, recebePass6 = 0, recebePass7 = 0, recebePass8 = 0, recebePass9 = 0, recebePass10 = 0;
-int inicio1, inicio2, inicio3, inicio4, inicio5, inicio6, inicio7, inicio8, inicio9, inicio10, resto1 = 0, inicio;
 int vagao11 = 45, vagao12 = 45, vagao13 = 45, vagao14 = 45, vagao15 = 45, vagao16 = 45, vagao17 = 45, vagao18 = 45, vagao19 = 45, vagao20 = 45;
 int trocoEconomica = 0, trocoExecutiva = 0, saque, resposta, troco, classeEconomica = 0, classeExecutiva = 0, passagem = 0, passagem2 = 0;
 float  classeEconomicaCartao = 0, classeExecutivaCartao = 0, receberDinheiro, trocoCliente = 0, trocoCliente2;
@@ -158,11 +152,12 @@ void cadastroTrem() {
 	gotoxy(9, 4); printf(" Classe Executiva "); cor(15); printf(" 1 a 5 "); cor(192);
 	gotoxy(9, 33); printf(" Classe Econ%cmica ", 147); cor(15); printf(" 6 a 10 ");
 	gotoxy(10, 10); cor(12); printf("Escolha a classe a partir dos n%cmeros: ", 163);
-	gotoxy(12, 2); cor(192); printf("Economica(3) = R$ 35,00 . Executiva(4) R$ 58,00:");
+	gotoxy(12, 2); cor(192); printf("Econ%cmica(3) = R$ 35,00 . Executiva(4) R$ 58,00:", 147);
+
+	gotoxy(14, 2); cor(192); printf("Dinheiro (1) . Cart%co (2):", 198);
+
 	cor(15); gotoxy(12, 51); scanf_s("%i", &resposta);
-	gotoxy(14, 2); cor(192); printf("Dinheiro (1) . Cartao (2):");
 	cor(15); gotoxy(14, 29); scanf_s("%i", &troco);
-	
 	gotoxy(5, 10); cor(15); scanf_s(" %[^\n]s", &passageiros, 200); getchar();
 	gotoxy(7, 39); scanf_s(" %i", &passagem1);
 	gotoxy(10, 49); scanf_s(" %i", &op);
@@ -170,41 +165,48 @@ void cadastroTrem() {
 	passagem = passagem1[0];
 	passagem2 = passagem1[0];
 
-	if (resposta == 3) 
+	if (resposta == 3)
 	{
 		passagem *= 35;
-		cor(192);  gotoxy(14, 38); printf("Total = %i", passagem);
+		cor(192);  gotoxy(14, 38); printf("Total = %i reais", passagem);
 	}
 	if (resposta == 4)
 	{
 		passagem2 *= 58;
-		cor(192); gotoxy(14, 38); printf("Total = %i", passagem2);
+		cor(192); gotoxy(14, 38); printf("Total = %i reais", passagem2);
 	}
 
-	cor(192); gotoxy(17, 2); printf("Valor a pagar:");
-	cor(15); gotoxy(17, 17); scanf_s("%f", &receberDinheiro);
+	if (troco != 2)
+	{
+		cor(192); gotoxy(16, 2); printf("Valor a pagar:");
+		cor(15); gotoxy(16, 17); scanf_s("%f", &receberDinheiro);
+	}
 
 	op3 = 0;
 	op2 = 0;
 	op2 += passagem1[0];
 	op3 += passagem1[0];
 
-	gotoxy(21, 1); printf("\t\t      "); cor(192); printf(" Seus assentos \n"); cor(15);
+	gotoxy(20, 1); printf("\t\t      "); cor(192); printf(" Seus assentos \n"); cor(15);
 
 	if (op == 1) {
 		printf("\n\t\t"); cor(12); printf("Vag%co 1 da Classe Executiva:", 198);
 
 		for (i = 0; i < op3; i++) {
+			if (pass1 == 45 || pass1 > 45)
+				break;
 			vagao11 = vagao1 - passagem1[0];
 			pass1++;
 			cor(15); printf("\n\t\t\tAssento %i", pass1);
-			if (pass1 == 45)
-				break;
-		}
 
-		if (op3 > 45) {
+		}
+		op4 += op2;
+		if (op4 > 45) {
+
 			printf("\n\n\t\t"); cor(12); printf("Vag%co 2 da Classe Executiva:", 198);
 			for (i = pass1; i < op3; i++) {
+				if (pass2 == 45 || pass2 > 45)
+					break;
 				pass2++;
 				vagao12 = vagao2 - pass2;
 				cor(15); printf("\n\t\t\tAssento %i", pass2);
@@ -217,16 +219,18 @@ void cadastroTrem() {
 		printf("\n\t\t"); cor(12); printf("Vag%co 2 da Classe Executiva:", 198);
 
 		for (i = 0; i < op2; i++) {
+			if (pass2 == 45 || pass2 > 45)
+				break;
 			vagao12 = vagao2 - passagem1[0];
 			pass2++;
 			cor(15); printf("\n\t\t\tAssento %i", pass2);
-			if (pass2 == 45)
-				break;
 		}
 
 		if (op3 > 45 || pass2 > 45) {
 			printf("\n\n\t\t"); cor(12); printf("Vag%co 3 da Classe Executiva:", 198);
 			for (i = pass2; i < op2; i++) {
+				if (pass3 == 45 || pass3 > 45)
+					break;
 				pass3++;
 				vagao13 = vagao2 - pass3;
 				cor(15); printf("\n\t\t\tAssento %i", pass3);
@@ -239,16 +243,18 @@ void cadastroTrem() {
 		printf("\n\t\t"); cor(12); printf("Vag%co 3 da Classe Executiva:", 198);
 
 		for (i = 0; i < op3; i++) {
+			if (pass3 == 45 || pass3 > 45)
+				break;
 			vagao13 = vagao3 - passagem1[0];
 			pass3++;
 			cor(15); printf("\n\t\t\tAssento %i", pass3);
-			if (pass3 == 45)
-				break;
 		}
 
 		if (op3 > 45 || pass3 > 45) {
 			printf("\n\n\t\t"); cor(12); printf("Vag%co 4 da Classe Executiva:", 198);
 			for (i = pass3; i < op3; i++) {
+				if (pass4 == 45 || pass4 > 45)
+					break;
 				pass4++;
 				vagao14 = vagao2 - pass4;
 				cor(15); printf("\n\t\t\tAssento %i", pass4);
@@ -261,16 +267,18 @@ void cadastroTrem() {
 		printf("\n\t\t"); cor(12); printf("Vag%co 4 da Classe Executiva:", 198);
 
 		for (i = 0; i < op3; i++) {
+			if (pass4 == 45 || pass4 > 45)
+				break;
 			vagao14 = vagao4 - passagem1[0];
 			pass4++;
 			cor(15); printf("\n\t\t\tAssento %i", pass4);
-			if (pass4 == 45)
-				break;
 		}
 
 		if (op3 > 45 || pass4 > 45) {
 			printf("\n\n\t\t"); cor(12); printf("Vag%co 5 da Classe Executiva:", 198);
 			for (i = pass4; i < op3; i++) {
+				if (pass5 == 45 || pass5 > 45)
+					break;
 				pass5++;
 				vagao15 = vagao2 - pass5;
 				cor(15); printf("\n\t\t\tAssento %i", pass5);
@@ -283,16 +291,18 @@ void cadastroTrem() {
 		printf("\n\t\t"); cor(12); printf("Vag%co 5 da Classe Executiva:", 198);
 
 		for (i = 0; i < op3; i++) {
+			if (pass5 == 45 || pass5 > 45)
+				break;
 			vagao15 = vagao5 - passagem1[0];
 			pass5++;
 			cor(15); printf("\n\t\t\tAssento %i", pass5);
-			if (pass5 == 45)
-				break;
 		}
 
 		if (op3 > 45 || pass5 > 45) {
-			printf("\n\n\t\t"); cor(12); printf("Vag%co 1 da Classe Econ%cmica:", 198,147);
+			printf("\n\n\t\t"); cor(12); printf("Vag%co 1 da Classe Econ%cmica:", 198, 147);
 			for (i = pass5; i < op3; i++) {
+				if (pass6 == 45 || pass6 > 45)
+					break;
 				pass6++;
 				vagao16 = vagao2 - pass6;
 				cor(15); printf("\n\t\t\tAssento %i", pass6);
@@ -302,19 +312,21 @@ void cadastroTrem() {
 	}
 
 	if (op == 6) {
-		printf("\n\t\t"); cor(12); printf("Vag%co 1 da Classe Econ%cmica:", 198,147);
+		printf("\n\t\t"); cor(12); printf("Vag%co 1 da Classe Econ%cmica:", 198, 147);
 
 		for (i = 0; i < op3; i++) {
+			if (pass6 == 45 || pass6 > 45)
+				break;
 			vagao16 = vagao6 - passagem1[0];
 			pass6++;
 			cor(15); printf("\n\t\t\tAssento %i", pass6);
-			if (pass6 == 45)
-				break;
 		}
 
 		if (op3 > 45 || pass6 > 45) {
-			printf("\n\n\t\t"); cor(12); printf("Vag%co 2 da Classe Econ%cmica:", 198,147);
+			printf("\n\n\t\t"); cor(12); printf("Vag%co 2 da Classe Econ%cmica:", 198, 147);
 			for (i = pass6; i < op3; i++) {
+				if (pass7 == 45 || pass7 > 45)
+					break;
 				pass7++;
 				vagao17 = vagao2 - pass7;
 				cor(15); printf("\n\t\t\tAssento %i", pass7);
@@ -324,19 +336,21 @@ void cadastroTrem() {
 	}
 
 	if (op == 7) {
-		printf("\n\t\t"); cor(12); printf("Vag%co 2 da Classe Econ%cmica:", 198,147);
+		printf("\n\t\t"); cor(12); printf("Vag%co 2 da Classe Econ%cmica:", 198, 147);
 
 		for (i = 0; i < op3; i++) {
+			if (pass7 == 45 || pass7 > 45)
+				break;
 			vagao17 = vagao7 - passagem1[0];
 			pass7++;
 			cor(15); printf("\n\t\t\tAssento %i", pass7);
-			if (pass7 == 45)
-				break;
 		}
 
 		if (op3 > 45 || pass7 > 45) {
-			printf("\n\n\t\t"); cor(12); printf("Vag%co 3 da Classe Econ%cmica:", 198,147);
+			printf("\n\n\t\t"); cor(12); printf("Vag%co 3 da Classe Econ%cmica:", 198, 147);
 			for (i = pass7; i < op3; i++) {
+				if (pass8 == 45 || pass8 > 45)
+					break;
 				pass8++;
 				vagao18 = vagao2 - pass8;
 				cor(15); printf("\n\t\t\tAssento %i", pass8);
@@ -346,19 +360,21 @@ void cadastroTrem() {
 	}
 
 	if (op == 8) {
-		printf("\n\t\t"); cor(12); printf("Vag%co 3 da Classe Econ%cmica:", 198,147);
+		printf("\n\t\t"); cor(12); printf("Vag%co 3 da Classe Econ%cmica:", 198, 147);
 
 		for (i = 0; i < op3; i++) {
+			if (pass8 == 45 || pass8 > 45)
+				break;
 			vagao18 = vagao8 - passagem1[0];
 			pass8++;
 			cor(15); printf("\n\t\t\tAssento %i", pass8);
-			if (pass4 == 45)
-				break;
 		}
 
 		if (op3 > 45 || pass8 > 45) {
-			printf("\n\n\t\t"); cor(12); printf("Vag%co 4 da Classe Econ%cmica:", 198,147);
+			printf("\n\n\t\t"); cor(12); printf("Vag%co 4 da Classe Econ%cmica:", 198, 147);
 			for (i = pass8; i < op3; i++) {
+				if (pass9 == 45 || pass9 > 45)
+					break;
 				pass9++;
 				vagao19 = vagao2 - pass9;
 				cor(15); printf("\n\t\t\tAssento %i", pass9);
@@ -368,19 +384,21 @@ void cadastroTrem() {
 	}
 
 	if (op == 9) {
-		printf("\n\t\t"); cor(12); printf("Vag%co 4 da Classe Econ%cmica:", 198,147);
+		printf("\n\t\t"); cor(12); printf("Vag%co 4 da Classe Econ%cmica:", 198, 147);
 
 		for (i = 0; i < op3; i++) {
+			if (pass9 == 45 || pass9 > 45)
+				break;
 			vagao19 = vagao9 - passagem1[0];
 			pass9++;
 			cor(15); printf("\n\t\t\tAssento %i", pass9);
-			if (pass9 == 45)
-				break;
 		}
 
 		if (op3 > 45 || pass9 > 45) {
-			printf("\n\n\t\t"); cor(12); printf("Vag%co 5 da Classe Econ%cmica:", 198,147);
+			printf("\n\n\t\t"); cor(12); printf("Vag%co 5 da Classe Econ%cmica:", 198, 147);
 			for (i = pass9; i < op3; i++) {
+				if (pass10 == 45 || pass10 > 45)
+					break;
 				pass10++;
 				cor(15); printf("\n\t\t\tAssento %i", pass10);
 			}
@@ -389,21 +407,21 @@ void cadastroTrem() {
 	}
 
 	if (op == 10) {
-		printf("\n\t\t"); cor(12); printf("Vag%co 5 da Classe Econ%cmica:", 198,147);
+		printf("\n\t\t"); cor(12); printf("Vag%co 5 da Classe Econ%cmica:", 198, 147);
 
 		for (i = 0; i < op3; i++) {
+			if (pass10 == 45 || pass10 > 45)
+				break;
 			vagao20 = vagao10 - passagem1[0];
 			pass10++;
 			cor(15); printf("\n\t\t\tAssento %i", pass10);
-			if (pass10 == 45)
-				break;
 		}
 		vagao10 -= passagem1[0];
 	}
 
 	printf("\n\n\n");
 	sistemaTroco();
-	cor(15); gotoxy(19, 9); system("pause");
+	cor(15); gotoxy(18, 9); system("pause");
 	system("cls");
 }
 
@@ -418,7 +436,7 @@ void cabecalho() {
 	printf("%c", 187);					// Canto superior direito do cabeçalho
 
 	printf("\n%c", 186);				// Traço vertical esquerdo do cabeçalho
-	gotoxy(2, 22); printf("EMPRESA TREM GRAND ", 210);	// CABEÇALHO
+	gotoxy(2, 22); printf("EMPRESA TREM GRAND%c ", 210);	// CABEÇALHO
 	gotoxy(2, 60); printf("%c", 186);	// Traço vertical direito do cabeçalho
 
 	printf("\n%c", 200);				// Canto inferior esquerdo do cabeçalho
@@ -627,7 +645,7 @@ void tabelaComprar() {
 	}
 	printf("%c", 217); // Canto inferior direito da tabela de dados
 
-	for (i = 11; i <= 17; i++)
+	for (i = 11; i <= 16; i++)
 	{
 		gotoxy(i, 1); printf("%c", 179);
 	}
@@ -640,15 +658,15 @@ void tabelaComprar() {
 
 	for (i = 2; i < 60; i++)
 	{
-		gotoxy(18, i); printf("%c", 196);
+		gotoxy(17, i); printf("%c", 196);
 	}
-	gotoxy(18, 1); printf("%c", 192);
-	gotoxy(18, 60); printf("%c", 217);
+	gotoxy(17, 1); printf("%c", 192);
+	gotoxy(17, 60); printf("%c", 217);
 
 	gotoxy(12, 60); printf("%c", 179);
 	gotoxy(14, 60); printf("%c", 179);
 	gotoxy(16, 60); printf("%c", 179);
-	gotoxy(17, 60); printf("%c", 179);
+	//gotoxy(17, 60); printf("%c", 179);
 
 	for (i = 2; i < 60; i++)
 	{
@@ -664,7 +682,7 @@ void tabelaComprar() {
 void tabelaMenu() {
 	cabecalho();
 	int j = 0;
-
+	LayoutTremzim();
 	gotoxy(4, 1); printf("%c", 218);		// Canto superior esquerdo da tabela de dados
 	for (j = 0; j < 58; j++) {
 		printf("%c", 196);				// Traço horizontal da tabela de dados
@@ -703,10 +721,30 @@ void tabelaMenu() {
 
 void LayoutTremzim()
 {
-	gotoxy(4, 65); printf("   ~~~~ ____   |~~~~~~~~~~~~~|");
-	gotoxy(5, 65); printf("  Y_,___|[]|   |   Equipe 4  |");
-	gotoxy(6, 65); printf(" {|_|_|_|PU|_,_|_____________|");
-	gotoxy(7, 65); printf("//oo---OO=OO     OOO     OOO");
+	gotoxy(5, 66); printf("   ~~~~ ____   |~~~~~~~~~~~~~|");
+	gotoxy(6, 66); printf("  Y_,___|[]|   |   Equipe 4  |");
+	gotoxy(7, 66); printf(" {|_|_|_|PU|_,_|_____________|");
+	gotoxy(8, 66); printf("//oo---OO=OO     OOO     OOO");
+	gotoxy(4, 63); printf("%c", 201);
+	for (i = 64; i <= 98; i++)
+	{
+		gotoxy(4, i); printf("%c", 205);
+	}
+	gotoxy(4, 99); printf("%c", 187);
+	for (i = 5; i <= 8; i++)
+	{
+		gotoxy(i, 99); printf("%c", 186);
+	}
+	for (i = 5; i <= 8; i++)
+	{
+		gotoxy(i, 63); printf("%c", 186);
+	}
+	gotoxy(9, 63); printf("%c", 200);
+	gotoxy(9, 99); printf("%c", 188);
+	for (i = 64; i <= 98; i++)
+	{
+		gotoxy(9, i); printf("%c", 205);
+	}
 }
 
 void gotoxy(int x, int y) {
@@ -729,7 +767,7 @@ void sistemaTroco()
 
 	if (troco == 1)
 	{
-		for (i = 0; i < op3; i++) 
+		for (i = 0; i < op3; i++)
 		{
 			if (pass6 || pass7 || pass8 || pass9 || pass10)
 				classeEconomica += 35;
@@ -745,9 +783,9 @@ void sistemaTroco()
 				classeEconomicaCartao += 35 - 1.75;
 
 		}
-		cor(192); gotoxy(17, 38); printf("Descontado = %.2f\n", classeEconomicaCartao);
+		cor(192); gotoxy(16, 38); printf("Descontado = %.2f\n", classeEconomicaCartao);
 	}
-	
+
 	if (troco == 2 && resposta == 4)
 	{
 		for (i = 0; i < op3; i++)
@@ -755,17 +793,17 @@ void sistemaTroco()
 			if (pass1 || pass2 || pass3 || pass4 || pass5)
 				classeExecutivaCartao += 58 - 1.75;
 		}
-		cor(192); gotoxy(17, 38); printf("Descontado = %.2f\n", classeExecutivaCartao);
+		cor(192); gotoxy(16, 38); printf("Descontado = %.2f\n", classeExecutivaCartao);
 	}
-	
+
 	if (troco == 1 && resposta == 3)
 	{
 		trocoCliente = receberDinheiro - classeEconomica;
-		cor(192); gotoxy(17, 38);  printf("Troco = %.2f\n", trocoCliente);
+		cor(192); gotoxy(16, 38);  printf("Troco = %.2f\n", trocoCliente);
 	}
-	if (troco == 1 && resposta == 4) 
+	if (troco == 1 && resposta == 4)
 	{
 		trocoCliente2 = receberDinheiro - classeExecutiva;
-		cor(192); gotoxy(17, 38); printf("Troco = %.2f\n", trocoCliente2);
+		cor(192); gotoxy(16, 38); printf("Troco = %.2f\n", trocoCliente2);
 	}
 }
