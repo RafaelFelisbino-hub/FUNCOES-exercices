3bfc64671bfa1a1d9705dafced4e1c8f8902f621
Rafael Felisbino Rocha da Silva <72631228+RafaelFelisbino-hub@users.noreply.github.com>
2021-09-11 14:58:11 -0300

    Deletar ocorrencias de uma letra em uma string

---
+++ ocorrencias.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+#define MAX_SIZE 100
+
+void ocorrencias(char *palavra, char const letra);
+
+int main(){
+    char palavra[MAX_SIZE], letra;
+
+    printf("Digite a palavra: ");
+    gets(palavra);
+
+    printf("Digite a letra: ");
+    letra = getchar();
+
+    ocorrencias(palavra,letra);
+
+    printf("Palavra removida: %c\nNova palavra = %s",letra, palavra);
+
+    return 0;
+}
+
+void ocorrencias(char *palavra, char const letra){
+  	int i, len, j;
+
+  	len = strlen(palavra);
+
+  	for(i = 0; i < len; i++){
+        if(palavra[i] == letra){
+            for(j = i; j < len; j++){
+                palavra[j] = palavra[j + 1];
+            }
+            len--;
+            i--;
+        }
+  	}
+}
